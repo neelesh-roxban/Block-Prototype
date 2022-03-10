0477abf1e3b48e50e62e6b04f6b9d0313a1bf22c
neelesh-roxban <roxbantv@gmail.com>
2022-03-10 19:07:39 +0530

    Scaling Updated

--- GameBerry/Assets/Scripts/Game/Grid/GridSquare.cs
+++ GameBerry/Assets/Scripts/Game/Grid/GridSquare.cs
@@ -7,9 +7,16 @@ public class GridSquare : MonoBehaviour
 {
     public Image normalImage;
     public List<Sprite> normalImages;
+    public Image hoverImage;
+    public Image activeImage;
+    public bool Selected {get; set;}
+    public int SquareIndex{get; set;}
+    public bool SquareOccupied {get; set;}
     
     void Start()
     {
+        Selected = false;
+        SquareOccupied = false;
         
     }
 
@@ -18,5 +25,23 @@ public class GridSquare : MonoBehaviour
     {
         normalImage.GetComponent<Image>().sprite= setFirstImage? normalImages[1] : normalImages[0];
 
+    }
+
+
+  
+    
+     public void OnTriggerEnter2D(Collider2D collision)
+    {
+        hoverImage.gameObject.SetActive(true);
+    }
+      public void OnTriggerStay2D(Collider2D collision)
+    {
+        hoverImage.gameObject.SetActive(true);
+
+    }
+     public void OnTriggerExit2D(Collider2D collision)
+    {
+        hoverImage.gameObject.SetActive(false);
+        
     }
 }
