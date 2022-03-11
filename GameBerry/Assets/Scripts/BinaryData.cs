784e81c11c526efd5c899c979c63dc000d82f2f8
neelesh-roxban <roxbantv@gmail.com>
2022-03-11 07:33:45 +0530

    High Score Added

---
+++ GameBerry/Assets/Scripts/BinaryData.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+public class BinaryData
+{
+
+    public static void Save<T>(T serilizedObject, string fileName)
+    {
+        string path = Application.persistentDataPath + "/SaveFile/";
+        Directory.CreateDirectory(path);
+
+
+        BinaryFormatter formater = new BinaryFormatter();
+        FileStream fileStreme = new FileStream(path+fileName+ ".dat", FileMode.Create);
+
+        try
+        {
+            formater.Serialize(fileStreme, serilizedObject);
+        }
+        catch (SerializationException e)
+        {
+            Debug.Log("Save Failed" + e.Message);           
+         
+        }
+
+        finally
+        {
+            fileStreme.Close();
+
+        }
+    }
+
+    public static bool Exists(string fileName)
+    {
+        string path=Application.persistentDataPath + "/SaveFile/";
+        string fullFileName = fileName+ ".dat";
+        return File.Exists(path + fullFileName);
+    }
+
+    public static T Read<T>(string fileName)
+    {
+        string path=Application.persistentDataPath + "/SaveFile/";
+         BinaryFormatter formater = new BinaryFormatter();
+         FileStream fileStreme = new FileStream(path+fileName+ ".dat", FileMode.Open);
+         
+         T returnType = default(T);
+
+        try
+        {
+            returnType = (T)formater.Deserialize(fileStreme);
+        }
+        catch (SerializationException e)
+        {
+            Debug.Log("Read Failed" + e.Message);           
+         
+        }
+
+        finally
+        {
+            fileStreme.Close();
+
+        }
+
+        return returnType;
+
+
+    }
+    
+}
--- GameBerry/Assets/Scripts/Scores.cs
+++ GameBerry/Assets/Scripts/Scores.cs
@@ -4,40 +4,93 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+[System.Serializable]
+public class HighScore
+{
+    public int score =0;
+}
+
 public class Scores : MonoBehaviour
 {
     public GameObject scoreText;
+    public GameObject popScoreText;
+    public GameObject highScoreText;
+    public GameObject popHighScoreText;
+
+    private bool newBestScore =false;
+    private HighScore _highScore = new HighScore();
     private int _score;
+    private string HighScoreKey = "HSData";
 
-
-     void Start()
+    void Awake()
     {
+        // StartCoroutine(ReadDataFile());
+        if(BinaryData.Exists(HighScoreKey))
+        {
+            StartCoroutine(ReadDataFile());
+        }
+        
+    }
+
+    private IEnumerator ReadDataFile()
+    {
+        _highScore = BinaryData.Read<HighScore>(HighScoreKey);
+        yield return  new WaitForEndOfFrame();
+
+        Debug.Log(_highScore.score);
+    }
+    
+    
+
+
+
+    void Start()
+    {
+         newBestScore =false;
         _score=0;
           UpdateScoreText();
 
     }
 
+   
+
     public void OnEnable()
     {
         GameEvents.AddScore+= AddScore;
+        GameEvents.GameOver+= SaveScore;
 
     }
     public void OnDisable()
     {
          GameEvents.AddScore-= AddScore;
+        GameEvents.GameOver-= SaveScore;
 
+    }
+
+    private void SaveScore(bool newBestScore)
+    {
+        BinaryData.Save<HighScore>(_highScore,HighScoreKey);
     }
 
     private void AddScore(int score)
     {
 
         _score +=score;
+        if(_score > _highScore.score)
+        {
+            newBestScore =true;
+            _highScore.score = _score;
+            
+        }
         UpdateScoreText();
     }
 
     private void UpdateScoreText()
     {
         scoreText.GetComponent<TMPro.TextMeshProUGUI>().text= _score.ToString();
+        popScoreText.GetComponent<Text>().text= _score.ToString();
+        highScoreText.GetComponent<TMPro.TextMeshProUGUI>().text= _highScore.score.ToString();
+        popHighScoreText.GetComponent<Text>().text=_highScore.score.ToString();
     }
 
    
